294c30c695087ed05f3bcde39f4da37951676bb7
Pang_GJ <2362421192@qq.com>
2022-01-14 20:56:45 +0800

    simplest SQL compiler and Virtual Machine

--- REPL.c
+++ REPL.c
@@ -10,10 +10,27 @@ typedef struct {
   ssize_t input_length;
 } InputBuffer;
 
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECGNIZED_STATEMENT } PrepareResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+  StatementType type;
+} Statement;
+
 InputBuffer *new_input_buffer();
 void print_prompt();
 void read_input(InputBuffer *input_buffer);
 void delete_input_buffer(InputBuffer *input_buffer);
+MetaCommandResult do_meta_command(InputBuffer *input_buffer);
+PrepareResult Prepare_statement(InputBuffer *input_buffer,
+                                Statement *statement);
+void execute_statement(Statement *statement);
 
 int main(int argc, char *argv[]) {
   InputBuffer *input_buffer = new_input_buffer();
@@ -26,12 +43,30 @@ int main(int argc, char *argv[]) {
     // 读取输入
     read_input(input_buffer);
 
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      delete_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    // 处理元命令
+    if (input_buffer->buffer[0] == '.') {
+      switch (do_meta_command(input_buffer)) {
+      case (META_COMMAND_SUCCESS):
+        continue;
+      case (META_COMMAND_UNRECGNIZED_COMMAND):
+        printf("Unrecgnized command '%s' \n", input_buffer->buffer);
+        continue;
+      }
     }
+
+    // 处理语句
+    Statement statement;
+    switch (Prepare_statement(input_buffer, &statement)) {
+    case PREPARE_SUCCESS:
+      break;
+    case PREPARE_UNRECGNIZED_STATEMENT:
+      printf("Unrecgnized keyword at start of '%s'. \n", input_buffer->buffer);
+      continue;
+    }
+
+    // 执行语句
+    execute_statement(&statement);
+    printf("Executed.\n");
   }
   return 0;
 }
@@ -63,4 +98,39 @@ void read_input(InputBuffer *input_buffer) {
 void delete_input_buffer(InputBuffer *input_buffer) {
   free(input_buffer->buffer);
   free(input_buffer);
+}
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    delete_input_buffer(input_buffer);
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECGNIZED_COMMAND;
+  }
+}
+
+// 简略的 SQL 解析器
+PrepareResult Prepare_statement(InputBuffer *input_buffer,
+                                Statement *statement) {
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  if (strcmp(input_buffer->buffer, "select") == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement) {
+  switch (statement->type) {
+  case (STATEMENT_INSERT):
+    printf("This is where we would do an insert.\n");
+    break;
+  case (STATEMENT_SELECT):
+    printf("This is where we would do a select.\n");
+    break;
+  }
 }
